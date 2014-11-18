73bb47bca4d4fef8d53ffc2ba5f8db89afe0bf86
swadia <sswadi3@gmail.com>
2014-11-18 23:42:26 +0100

    new commit

---
+++ src/main/java/com/mycompany/mavenproject/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject;
+
+/**
+ *
+ * @author WADII
+ */
+public class Main {
+    public static void main(String[] args) {
+        Module_A a = new Module_A();
+        System.out.println(a.fn());
+    }
+}
---
+++ src/main/java/com/mycompany/mavenproject/Module_A.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject;
+
+/**
+ *
+ * @author WADII
+ */
+public class Module_A {
+    
+    public int fn(){
+        return 1;
+    }
+          
+}
